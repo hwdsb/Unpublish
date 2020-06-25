acbe87e5235d6d042e00551cd3d5fe8c6a339d1a
r-a-y <r-a-y@gmx.com>
2020-06-25 14:53:03 -0700

    Gutenberg: Better formatting when meta box is displayed on the side.

--- inc/templates/unpublish-ui.tpl.php
+++ inc/templates/unpublish-ui.tpl.php
@@ -8,38 +8,42 @@
 	<fieldset id="unpublish-timestampdiv" class="hide-if-js">
 		<legend class="screen-reader-text"><?php esc_html_e( 'Unpublish Date and time', 'unpublish' ); ?></legend>
 		<div class="unpublish-timestamp-wrap">
-			<label>
-				<span class="screen-reader-text"><?php esc_html_e( 'Month', 'unpublish' ); ?></span>
-				<select id="unpublish-mm" name="unpublish-mm">
-					<option value=""><?php esc_html_e( '&mdash;', 'unpublish' ); ?></option>
-					<?php foreach ( $month_names as $month ) : ?>
-						<?php printf(
-							'<option value="%s" data-text="%s"%s>%s</option>',
-							esc_attr( $month['value'] ),
-							esc_attr( $month['text'] ),
-							selected( $date_parts['mm'], $month['value'], false ),
-							esc_html( $month['label'] )
-						); ?>
-					<?php endforeach; ?>
-				</select>
-			</label>
-			<label>
-				<span class="screen-reader-text"><?php esc_html_e( 'Day', 'unpublish' ); ?></span>
-				<input id="unpublish-jj" name="unpublish-jj" size="2" maxlength="2" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['jj'] ); ?>" />
-			</label>,<label>
-				<span class="screen-reader-text"><?php esc_html_e( 'Year', 'unpublish' ); ?></span>
-				<input id="unpublish-aa" name="unpublish-aa" size="4" maxlength="4" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['aa'] ); ?>" />
-			</label>
-			@
-			<label>
-				<span class="screen-reader-text"><?php esc_html_e( 'Hour', 'unpublish' ); ?></span>
-				<input id="unpublish-hh" name="unpublish-hh" size="2" maxlength="2" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['hh'] ); ?>" />
-			</label>
-			:
-			<label>
-				<span class="screen-reader-text"><?php esc_html_e( 'Minute', 'unpublish' ); ?></span>
-				<input id="unpublish-mn" name="unpublish-mn" size="2" maxlength="2" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['mn'] ); ?>" />
-			</label>
+			<div class="unpublish-date">
+				<label>
+					<span class="screen-reader-text"><?php esc_html_e( 'Month', 'unpublish' ); ?></span>
+					<select id="unpublish-mm" name="unpublish-mm">
+						<option value=""><?php esc_html_e( '&mdash;', 'unpublish' ); ?></option>
+						<?php foreach ( $month_names as $month ) : ?>
+							<?php printf(
+								'<option value="%s" data-text="%s"%s>%s</option>',
+								esc_attr( $month['value'] ),
+								esc_attr( $month['text'] ),
+								selected( $date_parts['mm'], $month['value'], false ),
+								esc_html( $month['label'] )
+							); ?>
+						<?php endforeach; ?>
+					</select>
+				</label>
+				<label>
+					<span class="screen-reader-text"><?php esc_html_e( 'Day', 'unpublish' ); ?></span>
+					<input id="unpublish-jj" name="unpublish-jj" size="2" maxlength="2" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['jj'] ); ?>" />
+				</label>,<label>
+					<span class="screen-reader-text"><?php esc_html_e( 'Year', 'unpublish' ); ?></span>
+					<input id="unpublish-aa" name="unpublish-aa" size="4" maxlength="4" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['aa'] ); ?>" />
+				</label>
+				@
+			</div>
+			<div class="unpublish-time">
+				<label>
+					<span class="screen-reader-text"><?php esc_html_e( 'Hour', 'unpublish' ); ?></span>
+					<input id="unpublish-hh" name="unpublish-hh" size="2" maxlength="2" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['hh'] ); ?>" />
+				</label>
+				:
+				<label>
+					<span class="screen-reader-text"><?php esc_html_e( 'Minute', 'unpublish' ); ?></span>
+					<input id="unpublish-mn" name="unpublish-mn" size="2" maxlength="2" autocomplete="off" type="text" value="<?php echo esc_attr( $date_parts['mn'] ); ?>" />
+				</label>
+			</div>
 			<p>
 				<a href="#edit-unpublish-timestamp" class="save-unpublish-timestamp hide-if-no-js button"><?php esc_html_e( 'OK', 'unpublish' ); ?></a>
 				<a href="#edit-unpublish-timestamp" class="cancel-unpublish-timestamp hide-if-no-js button-cancel"><?php esc_html_e( 'Cancel', 'unpublish' ); ?></a>
